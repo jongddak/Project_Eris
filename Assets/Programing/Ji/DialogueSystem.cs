c13b1157a1e198a13cd1c20117952eb0a9429a7b
YIJEUN <yijeun.dev@gmail.com>
2024-10-27 00:21:01 +0900

    [feat] 대화 시스템 프로토타이핑
    - 캐릭터 대화 시스템의 UI구성
    - 캐릭터 대화 시스템의 소스 코드를 의사 코드 형태로 일부 구현
    - 관련 자료 추가

---
+++ Assets/Programing/Ji/DialogueSystem.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class DialogueSystem : MonoBehaviour
+{
+    /* 대사 스크립트 관련 유튜브 영상
+     * https://www.youtube.com/watch?v=1fRbGvQlIEQ
+     * https://www.youtube.com/watch?v=_04sCWLHoXU
+     * https://www.youtube.com/watch?v=qJjfYvEYKiE
+     * 
+     * 대사를 타자형식으로 출력하는 것 관련 유튜브 영상
+     * https://www.youtube.com/watch?v=OjcPuEVQT6s
+     */
+
+    // 함수의 종류는 영상을 보고 공부하여 변동 가능성 있음
+    public void StartDialogue()
+    {
+        // 씬이 시작하는 순간에 대사들을 출력하는 함수
+    }
+    public void Phase1Dialogue()
+    {
+        // Phase1이 끝나고 몬스터의 상태가 변하면 출력되어야하는 대사를 출력하는 함수
+    }
+    public void Phase2Dialogue()
+    {
+        // Phase2가 끝나고 몬스터가 사망하면 출력되어야하는 함수
+    }
+}
+
+
--- Assets/Programing/Ji/SceneManager.cs
+++ Assets/Programing/Ji/SceneManager.cs
@@ -1,8 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneManager : MonoBehaviour
 {
-    
+    // Scene의 상태를 저장
+    public enum SceneState { Start, Phase1, Phase2 };
+    /* Start : Scene에 들어온 순간, 시작하자마자의 상태
+     * Phase1 : 대사를 전부 출력한 후 Phase1 시작
+     *          - Phase1이면서 보스의 상태가 Phase2로 변할 때 Phase2로 변동
+     *          - Boss의 Phase를 씬으로 분리할지, 애니메이션과 상태로만 분리할지에 따라 달라져야함
+     * Phase2 : Phase2로 변하자마자 보스의 대사 스크립트를 출력
+     *          - Phase2일 때, 보스가 사망하면 사망 스크립트를 출력해야 함
+     */
+    public SceneState nowSceneState; // 현재 씬의 상태를 저장
+
+    [SerializeField] DialogueSystem dialogueSystem; // DialogueSystem.cs를 할당
+
+    private void Awake()
+    {
+        nowSceneState = SceneState.Start; // 씬이 시작하자마자 Start 상태로 시작
+    }
+
+    private void Update()
+    {
+        // 현재 씬의 상태에 따라서 진행하는 함수로 구현 - 상태패턴
+        switch (nowSceneState)
+        {
+            case SceneState.Start:
+                dialogueSystem.StartDialogue(); // DialogueSystem.cs에서 시작시 출력하는 대사
+                break;
+            case SceneState.Phase1:
+                dialogueSystem.Phase1Dialogue(); // DialogueSystem.cs에서 Phase1 종료시 출력하는 대사
+                break;
+            case SceneState.Phase2:
+                dialogueSystem.Phase2Dialogue(); // DialogueSystem.cs에서 Phase2 종료시 출력하는 대사
+                break;
+        }
+
+    }
 }
