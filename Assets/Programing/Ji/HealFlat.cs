28f94293d9d54270d5d686e6f3ee3d2a8eed711e
YIJEUN <yijeun.dev@gmail.com>
2024-10-27 11:18:12 +0900

    [feat] HealFlat 깜빡이 조절
    - HealFlat이 충돌한 직후부터 삭제되기 전까지 깜빡이도록 구현

--- Assets/Programing/Ji/HealFlat.cs
+++ Assets/Programing/Ji/HealFlat.cs
@@ -7,6 +7,7 @@ public class HealFlat : MonoBehaviour
     // 시간 조절 후 오브젝트 삭제
     [Header("State")]
     [SerializeField] float DeleteTime; // 삭제까지 걸리는 시간 조절
+    [SerializeField] SpriteRenderer spriteRenderer; // 발판의 이미지
     /*
         /// <summary>
         /// HealFlat에 충돌체가 충돌하는 순간 판단
@@ -53,9 +54,23 @@ public class HealFlat : MonoBehaviour
         StartCoroutine(FlatDelete()); // 코루틴으로 시간 조절
     }
 
+    /// <summary>
+    /// HealFlat의 깜빡임을 조절하는 코루팀
+    /// </summary>
+    /// <returns></returns>
     IEnumerator FlatDelete()
     {
-        yield return new WaitForSeconds(DeleteTime);
+        float timeTerm = 0.5f; // 깜빡이는 간격을 조정하는 타이머
+        while (DeleteTime > 0) // 지워지려는 시간이 0보다 큰 동안에 깜빡임 반복
+        {
+            spriteRenderer.color = new Color(1, 1, 1, 0.5f); // 이미지의 투명도 50% 설정
+            yield return new WaitForSeconds(timeTerm); // 일정시간 유지
+            DeleteTime -= timeTerm; // timeTerm이 지나간 만큼 DeletTime 감소 
+            spriteRenderer.color = new Color(1, 1, 1, 1f); // 이미지의 투명도 100% 설정
+            yield return new WaitForSeconds(timeTerm); // 일정시간 유지
+            DeleteTime -= timeTerm; // timeTerm이 지나간 만큼 DeletTime 감소 
+        }
+        // DeleteTime 이 0이되면 오브젝트를 삭제함
         Destroy(gameObject); // 삭제까지 시간이 지나면 HealFlat 오브젝트 삭제
     }
 }
