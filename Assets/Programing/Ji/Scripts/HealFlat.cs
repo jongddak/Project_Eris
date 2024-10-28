ed32dbd0febd9f1f1d3f1764357c1adf6437daed
YIJEUN <yijeun.dev@gmail.com>
2024-10-28 16:41:59 +0900

    [feat] Map_Boss2Stage 이미지 작업
    - Boss2Stage의 기존의 이미지의 사이즈와 비율을 조절하여 이미지 작업을 완료
      - Sorting Layer를 사용하여 배경, 오브젝트, 캐릭터 간의
        원근감 출력
      - 추후 기획팀의 컨펌 후 완전히 완료 할 예정
    - 이미지 작업과 함께 Boss2Stage의 충돌체 Layer와 tag의 재설정
      - 추후 일괄적으로 물리 충돌 체크 후 완료 할 예정

--- Assets/Programing/Ji/Scripts/HealFlat.cs
+++ Assets/Programing/Ji/Scripts/HealFlat.cs
@@ -8,50 +8,29 @@ public class HealFlat : MonoBehaviour
     [Header("State")]
     [SerializeField] float DeleteTime; // 삭제까지 걸리는 시간 조절
     [SerializeField] SpriteRenderer spriteRenderer; // 발판의 이미지
-    /*
-        /// <summary>
-        /// HealFlat에 충돌체가 충돌하는 순간 판단
-        /// </summary>
-        /// <param name="collision"></param>
-        private void OnCollisionEnter2D(Collision2D collision)
-        {
-            if (collision.gameObject.tag == "Finsh") // 추후 PlayerFoot 태그 추가 하여 변경 필요
-            {
-                /* tag가 PlayerFoot인 경우
-                 * 플레이어가 밟은 순간 카운팅 활성화
-                 * 특정 카운트가 지나면 오브젝트 삭제하도록
-                 * HealFlat.cs에서 함수 작성해 불러오기
-                 *
-                StartCoroutine(FlatDelete());
-                // DeleteFlat(); // 삭제하는 함수 실행
-            }
-        }
-    */
 
     /// <summary>
-    /// HealFlat에 Trigger 충돌체가 충돌할 때
+    /// HealFlat에 충돌체가 충돌할 때
     /// </summary>
     /// <param name="collision"></param>
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag == "Finish") // 추후 PlayerFoot 태그 추가 하여 변경 필요
+        if (collision.gameObject.tag == "Player")
         {
-            /* tag가 PlayerFoot인 경우
-             * 플레이어가 밟은 순간 카운팅 활성화
-             * 특정 카운트가 지나면 오브젝트 삭제하도록
-             * HealFlat.cs에서 함수 작성해 불러오기
+            /* tag가 Player인 경우
+             * OnCollisionEnter가 발생하기 전의 collision의 relativeVelocity(충돌이전의 속도)를 가져와서
+             * relativeVelocity.y <0인 경우에만 삭제하는 코루틴을 출력하도록 함
+             * 
+             * 위에서 밟았을 때에만 코루틴이 작동해야하므로
              */
-            Debug.Log("충돌");
-            StartCoroutine(FlatDelete());
-            // DeleteFlat(); // 삭제하는 함수 실행
+            if (collision.relativeVelocity.y < 0)
+            {
+                // collision의 충돌 직전의 속도가 0보다 작다는 것은
+                // 충돌체가 위에서 아래로 내려오고 있다는 뜻이므로
+                // 충돌체가 위에서 밟았을 때에만 일정시간 후 삭제하는 코루틴 작동
+                StartCoroutine(FlatDelete()); 
+            }
         }
-    }
-    /// <summary>
-    /// 코루틴으로 시간을 조절한 후 현재 오브젝트 삭제
-    /// </summary>
-    public void DeleteFlat()
-    {
-        StartCoroutine(FlatDelete()); // 코루틴으로 시간 조절
     }
 
     /// <summary>
@@ -60,7 +39,7 @@ public class HealFlat : MonoBehaviour
     /// <returns></returns>
     IEnumerator FlatDelete()
     {
-        float timeTerm = 0.5f; // 깜빡이는 간격을 조정하는 타이머
+        float timeTerm = 0.3f; // 깜빡이는 간격을 조정하는 타이머 - 원하는 시간으로 조정 가능
         while (DeleteTime > 0) // 지워지려는 시간이 0보다 큰 동안에 깜빡임 반복
         {
             spriteRenderer.color = new Color(1, 1, 1, 0.5f); // 이미지의 투명도 50% 설정
