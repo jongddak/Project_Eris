5a4daaf947b95fed0ab6518eb645afbcd62163ed
jongddak <hfsmsj316@gmail.com>
2024-10-29 21:01:07 +0900

    fix 스크립트 주석 수정

--- Assets/Programing/Jong/Script/Boss3/Shooting.cs
+++ Assets/Programing/Jong/Script/Boss3/Shooting.cs
@@ -9,29 +9,44 @@ public class Shooting : MonoBehaviour
     [SerializeField] GameObject player;
 
 
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] GameObject missilePrefab;
+    [SerializeField] GameObject bulletPrefab;
+
+
 
     private void Update()
     {
         
     }
 
+    IEnumerator ShootingBullet() //총알 패턴 
+    {
+        while (true) 
+        {
+            yield return null;
+        }
+
+        
+    }
 
 
     IEnumerator ShootingLaser() //레이저 패턴
     {
         for (int i = 0; i < 6; i++) 
         {
-
+               
         }
 
-
+        yield return null;
     }
 
     IEnumerator ShootingMissile() // 미사일 패턴 
     {
         for (int i = 0; i < 3; i++) 
         {
-
+            int x = Random.Range(0, 7);
         }
+        yield return null;
     }
 }
