b3eb6716795112a96ef65d07e792c40fcdf9a8c6
jongddak <hfsmsj316@gmail.com>
2024-11-01 21:10:46 +0900

    보스 3 미사일, 레이저 기능 추가

--- Assets/Programing/Jong/Script/Boss3/Laser.cs
+++ Assets/Programing/Jong/Script/Boss3/Laser.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
+    [SerializeField] GameObject player;   
+    
+    private void Start()
+    {   
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        Vector3 playerPos = player.transform.position;
+
+        Vector2 direction = playerPos - transform.position;
+
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
 
     void Update()
     {
-        transform.Translate(Vector2.up * 10f * Time.deltaTime);
-    }
+        transform.Translate(Vector2.right * 10f * Time.deltaTime);
+    } 
 }
--- Assets/Programing/Jong/Script/Boss3/Line.cs
+++ Assets/Programing/Jong/Script/Boss3/Line.cs
@@ -18,9 +18,9 @@ public class Line : MonoBehaviour
     void Update()
     {
         lineRenderer.SetPosition(0, startPoint.position);
-
+        //lineRenderer.startColor = Color.red;
         Vector3 direction = (endPoint.position - startPoint.position).normalized; // 방향을 계산 
-        Vector3 endPointover = startPoint.position + direction * 30f; // 라인의 길이 
-        lineRenderer.SetPosition(1, endPointover);  // 계산된 방향의 길이만큼 뒤 까지 라인을 그림
+        Vector3 endPointover = startPoint.position + direction * 500f; // 라인의 길이 
+        lineRenderer.SetPosition(1, (Vector2)endPointover);  // 계산된 방향의 길이만큼 뒤 까지 라인을 그림
     }
 }
