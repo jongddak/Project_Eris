671f533c4d33e805e15e039c6d533e3cc2f467d3
YIJEUN <yijeun.dev@gmail.com>
2024-11-01 22:46:54 +0900

    [fix[ Boss3 플랫폼 밟기 수정
    - 플랫폼을 밟고 서는 경우 밀리도록 수정 완료

--- Assets/Programing/Ji/Scripts/Stage/Boss3/SlidePlatform.cs
+++ Assets/Programing/Ji/Scripts/Stage/Boss3/SlidePlatform.cs
@@ -4,10 +4,9 @@ using UnityEngine;
 
 public class SlidePlatform : MonoBehaviour
 {
-    float velocity;
-    Rigidbody2D playerRigidbody;
-    bool isLeft; // 플레이어가 서있는 방향을 판별
-    public bool isCollison = false; // 플레이어가 플렛폼과 충돌여부를 확인
+    [SerializeField] float velocity;
+    [SerializeField] Rigidbody2D playerRigidbody;
+    [SerializeField] public bool isLeft; // 플레이어가 서있는 방향을 판별
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -24,14 +23,16 @@ public class SlidePlatform : MonoBehaviour
     {
         if(collision.gameObject.tag == "Player")
         {
-            isCollison = true;
             if (isLeft) // 플레이어가 물체의 기준보다 왼쪽에 있는 경우
             {
+                Debug.Log($"왼쪽인경우 {velocity},{playerRigidbody.velocity.y}");
                 // 플레이어의 속력을 주어서 밀려나도록 설정
                 playerRigidbody.velocity = new Vector2(velocity, playerRigidbody.velocity.y);
             }
             else // 플레이어가 물체의 기준보다 오른쪽에 있는 경우
             {
+                Debug.Log($"오른쪽인경우 {velocity},{playerRigidbody.velocity.y}");
+
                 // 플레이어의 속력을 주어서 밀려나도록 설정 - 반대쪽으로
                 playerRigidbody.velocity = new Vector2(-velocity, playerRigidbody.velocity.y);
             }
