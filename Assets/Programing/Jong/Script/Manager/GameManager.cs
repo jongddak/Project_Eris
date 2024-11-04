7a910b838f1941a413fb3d31a93f8b3f90709dbb
jongddak <hfsmsj316@gmail.com>
2024-11-04 15:19:21 +0900

    feat 게임매니저 작성

--- Assets/Programing/Jong/Script/Manager/GameManager.cs
+++ Assets/Programing/Jong/Script/Manager/GameManager.cs
@@ -1,18 +1,68 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+    public AudioSource bgmSource;
+    public AudioClip[] bgmClips; // 각 씬에 맞는 BGM 클립을 설정
+    private bool isPaused = false;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        SceneManager.sceneLoaded += OnSceneLoaded;
+       
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) == true) 
+        {
+            PauseGame();
+        }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+       
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void PauseGame()
+    {
+        if (!isPaused)
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
