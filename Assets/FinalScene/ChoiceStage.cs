fa7ae9df54adfb02513f4f76cfab97d6bbe99fb6
YIJEUN <yijeun.dev@gmail.com>
2024-11-05 12:25:19 +0900

    [feat] 추가 Scene 생성 / Scene 이동 연결

---
+++ Assets/FinalScene/ChoiceStage.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class ChoiceStage : MonoBehaviour
+{
+    bool stage1;
+    bool stage2;
+    bool stage3;
+
+    private void Awake()
+    {
+        DataManager.Instance.LoadGameData(); // 불러오기
+        stage1 = DataManager.Instance.data.isUnlock[0];
+        stage2 = DataManager.Instance.data.isUnlock[1];
+        stage3 = DataManager.Instance.data.isUnlock[2];
+    }
+
+
+    /// <summary>
+    /// 트리거가 발생 중인 동안에
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            switch (gameObject.name) // 플레이어와 충돌한 경우 게임 오브젝트의 이름으로 분기
+            {
+                case "Stage1":
+                        if (Input.GetKeyDown(KeyCode.F))
+                        {
+                            GameManager.Instance.LoadSceneByName("Boss1DStart");
+                        }
+                    break;
+                case "Stage2":
+                    if (Input.GetKeyDown(KeyCode.F))
+                    {
+                        GameManager.Instance.LoadSceneByName("Boss2DStart");
+                    }
+                    break;
+                case "Stage3":
+                    if (Input.GetKeyDown(KeyCode.F))
+                    {
+                        GameManager.Instance.LoadSceneByName("Boss3DStart");
+                    }
+                    break;
+            }
+        }
+    }
+}
